a4be0f66d565a4cd4883a8fcfe6645602823f075
KnD0715 <rlaalswo0715@naver.com>
2025-07-20 22:24:15 +0900

    add file swea

---
+++ swea/swea6196.py
@@ -0,0 +1,10 @@
+N = int(input())
+NN = ""
+NNN = ""
+NNNN = ""
+
+NN = str(N) + str(N)
+NNN = str(N) + str(N) + str(N)
+NNNN = str(N) + str(N) + str(N) + str(N)
+
+print (N + int(NN) + int(NNN) + int(NNNN))
