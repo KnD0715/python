ae45fb1f304d93eb931e28c82235b6c09841633b
KnD0715 <rlaalswo0715@naver.com>
2025-10-01 00:28:09 +0900

    20251001

--- baekjoon/gold/test34472.py
+++ baekjoon/gold/test34472.py
@@ -4,3 +4,19 @@ N = int(sys.stdin.readline())
 room = list(map(int, input().split()))
 
 count = 0
+
+avg = sum(room) // N
+
+prefix = 0
+min_prefix = 0
+sum_prefix = 0
+
+for mem in room:
+    prefix += mem - avg
+    sum_prefix += prefix
+    if prefix < min_prefix:
+        min_prefix = prefix
+
+count = sum_prefix - N * min_prefix
+
+print(count)
