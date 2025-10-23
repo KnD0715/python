d6920d073bc9eeb2a0382037565086199321a241
KnD0715 <rlaalswo0715@naver.com>
2025-10-24 00:58:49 +0900

    20251024

---
+++ baekjoon/bronze/test5086.py
@@ -0,0 +1,16 @@
+import sys
+
+while True:
+    A, B = map(int, sys.stdin.readline().split())
+
+    if A == 0 & B == 0:
+        break
+
+    if A % B == 0:
+        print('multiple')
+
+    elif B % A == 0:
+        print('factor')
+
+    else:
+        print('neither')
