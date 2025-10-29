80b1950df0c3eb6f3f9a6a93fdc8722cb91165b2
KnD0715 <rlaalswo0715@naver.com>
2025-10-29 13:44:01 +0900

    20251029

---
+++ baekjoon/bronze/test1259.py
@@ -0,0 +1,13 @@
+import sys
+
+while True:
+    num = sys.stdin.readline().strip()
+
+    if num == '0':
+        break
+
+    if num == num[::-1]:
+        print('yes')
+
+    else:
+        print('no')
