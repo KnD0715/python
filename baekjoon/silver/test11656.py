4a7956d6fdb1de9b5502c4bb59f1995f18d5fc22
KnD0715 <rlaalswo0715@naver.com>
2025-11-09 20:29:54 +0900

    20251109

---
+++ baekjoon/silver/test11656.py
@@ -0,0 +1,12 @@
+import sys
+
+word = sys.stdin.readline().strip()
+word_list = []
+
+for i in range(len(word)):
+    word_list.append(word[i:])
+
+word_list.sort()
+
+for w in word_list:
+    print(w)
